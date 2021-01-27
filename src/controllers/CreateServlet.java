4d73db2b585c0dca2fd6ebaa27f5fe5833e75f9f
nagoyann9 <nagoyann9@gmail.com>
2021-01-27 22:09:46 +0900

    Bug fix

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -73,14 +73,6 @@ public class CreateServlet extends HttpServlet {
                 // indexのページにリダイレクト
                 response.sendRedirect(request.getContextPath() + "/index");
             }
-
-            em.getTransaction().begin();
-            em.persist(m);
-            em.getTransaction().commit();
-            request.getSession().setAttribute("flush", "Registrated.");
-            em.close();
-
-            response.sendRedirect(request.getContextPath() + "/index");
         }
     }
 
